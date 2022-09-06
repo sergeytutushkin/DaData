a7e4424c66c913e385225897cf3d72ee4b5f7117
sergeytutushkin <sergeytutushkin@gmail.com>
2022-09-07 02:01:34 +0300

    Added ViewBinding for SearchFragment

--- app/src/main/java/dev/tutushkin/dadata/ui/search/SearchFragment.kt
+++ app/src/main/java/dev/tutushkin/dadata/ui/search/SearchFragment.kt
@@ -6,14 +6,24 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import dev.tutushkin.dadata.R
+import dev.tutushkin.dadata.databinding.FragmentSearchBinding
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
     private val viewModel: SearchViewModel by viewModels()
 
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        _binding = FragmentSearchBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
